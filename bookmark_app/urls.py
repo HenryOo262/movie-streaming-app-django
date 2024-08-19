a5abe328a28204fde63f84bf523612b329cb6fec
Henry Oo <oohenry225@gmail.com>
2024-08-19 20:46:14 +0630

    bookmark_app.bookmarks

--- bookmark_app/urls.py
+++ bookmark_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('movies/', views.bookmarks_movies, name='bookmark_app.bookmarks_movies'),
-    path('series/', views.bookmarks_series, name='bookmark_app.bookmarks_series'),
+    path('<str:content_type>/', views.bookmarks, name='bookmark_app.bookmarks'),
     path('bookmark/movie/<int:id>', views.movie_bookmark, name='bookmark_app.movie_bookmark'),
     path('bookmark/series/<int:id>/<int:current_season>/<int:current_episode>/<str:resolution>', views.series_bookmark, name='bookmark_app.series_bookmark'),
 ]
--- bookmark_app/views.py
+++ bookmark_app/views.py
@@ -8,15 +8,24 @@ from movie_app.models import Movie
 from series_app.models import Series
 
 
-def bookmarks_movies(request):
+def bookmarks(request, content_type):
     """ Returns Paginated Pages of Bookmarked movies """
-    movie_bookmarks = Bookmark.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(Movie))
-    paginator = Paginator(movie_bookmarks, 12)
+    if content_type == 'movies':
+        model = Movie
+    elif content_type == 'series':
+        model = Series
+
+    bookmarks = Bookmark.objects.filter(user=request.user, content_type=ContentType.objects.get_for_model(model))
+    paginator = Paginator(bookmarks, 12)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    
-    return render(request, 'bookmark.html', {"page_obj": page_obj, "content_type": "movies"})   
+
+    context = {
+        "page_obj": page_obj, 
+        "content_type": content_type,
+    } 
+    return render(request, 'bookmark.html', context)   
 
 
 def bookmarks_series(request):
